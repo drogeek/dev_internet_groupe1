97ea39170347d5cb132ba36735c8d169951a04b5
Deshors <yann.deshors@gmail.com>
2017-10-15 17:15:06 +0200

    Ajout de classes metiers et test volontairement faux (verification SonarQube)

---
+++ src/test/java/PokemonTest.java
@@ -0,0 +1,26 @@
+import fr.univtln.groupe1.metier.Dresseur;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.fail;
+
+public class PokemonTest {
+    @Test
+    public void ajoutDresseur(){
+        fail("En cours de création!");
+    }
+
+    @Test
+    public void suppDresseur(){
+        fail("En cours de création!");
+    }
+
+    @Test
+    public void niveauSupp(){
+        fail("En cours de création!");
+    }
+
+    @Test
+    public void expSupp(){
+        fail("En cours de création!");
+    }
+}
